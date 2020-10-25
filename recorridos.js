19faa4dd4ebe0d6f22c40d3f21aaa56afaf80e39
Jaime A Chaves <politologoamigo@gmail.comgit config --list--help>
2020-10-25 06:31:21 -0500

    Presentan dos métodos para recorrer un Array

---
+++ recorridos.js
@@ -0,0 +1,7 @@
+var articulos = [
+    {nombre: "bici", costo: 3000},
+    {nombre: "Tv", costo: 2500},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+]
